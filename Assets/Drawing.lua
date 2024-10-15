9d2628d1d531a369c92c3402dc33d7b394d15fde
yousef00112 <130393872+yousef00112@users.noreply.github.com>
2024-10-15 02:58:38 +0200

    Update Drawing.lua

--- Assets/Drawing.lua
+++ Assets/Drawing.lua
@@ -1,7 +1,7 @@
 local coreGui = game:GetService("CoreGui")
 local camera = workspace.CurrentCamera
 local drawingUI = Instance.new("ScreenGui")
-drawingUI.Name = "Drawing"
+drawingUI.Name = "Telchid DrawingLib"
 drawingUI.IgnoreGuiInset = true
 drawingUI.DisplayOrder = 0x7fffffff
 drawingUI.Parent = coreGui
